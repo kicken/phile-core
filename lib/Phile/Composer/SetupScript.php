06278937a1e583341d2e2a479f23a116c3ea7c09
Keith <keithm@aoeex.com>
2016-07-06 10:22:15 -0400

    Change back to non-static stuff, use composer to get root path.

--- lib/Phile/Composer/SetupScript.php
+++ lib/Phile/Composer/SetupScript.php
@@ -8,90 +8,95 @@
 
 namespace Phile\Composer;
 
+use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Script\Event;
 use Phile\Core\Utility;
 
 class SetupScript {
-    public static function run(Event $event)
-    {
-        $io = $event->getIO();
+    private $io;
+    private $composer;
 
-        $io->write('Welcome to PhileCMS');
-        $io->write('To continue we first need to get a few configuration details out of the way.');
-
-        $config = [
-            'site_title' => self::askSiteTitle($io)
-            , 'encryptionKey' => self::askEncryptionKey($io)
-        ];
-
-        self::writeConfiguration($io, $config);
-        self::createVarDirectory($io);
+    public function __construct(IOInterface $io, Composer $composer){
+        $this->io = $io;
+        $this->composer = $composer;
     }
 
-    private static function askSiteTitle(IOInterface $io)
-    {
+    public static function run(Event $event){
+        $script = new self($event->getIO(), $event->getComposer());
+        $script->execute();
+    }
+
+    private function execute(){
+        $this->io->write('Welcome to PhileCMS');
+        $this->io->write('To continue we first need to get a few configuration details out of the way.');
+
+        $config = [
+            'site_title' => $this->askSiteTitle()
+            , 'encryptionKey' => $this->askEncryptionKey()
+        ];
+
+        $this->writeConfiguration($config);
+        $this->createVarDirectory();
+    }
+
+    private function askSiteTitle(){
         $title = 'PhileCMS';
         $question = sprintf('<question>Site Title [%s]:</question> ', $title);
 
-        return $io->ask($question, $title);
+        return $this->io->ask($question, $title);
     }
 
-    private static function askEncryptionKey(IOInterface $io)
-    {
-        $key = self::generateRandomKey();
+    private function askEncryptionKey(){
+        $key = $this->generateRandomKey();
         $question = sprintf('<question>Encryption Key [%s]:</question>', '<random>');
 
-        return $io->ask($question, $key);
+        return $this->io->ask($question, $key);
     }
 
-    private static function generateRandomKey()
-    {
+    private function generateRandomKey(){
         return Utility::generateSecureToken(64);
     }
 
-    private static function getPath($sub)
-    {
-        $rootDir = __DIR__ . '/../';
+    private function getPath($sub){
+        $rootDir = $this->composer->getPackage()->getTargetDir();
 
-        return str_replace('/', DIRECTORY_SEPARATOR, $rootDir . $sub);
+        return str_replace('/', DIRECTORY_SEPARATOR, $rootDir . '/'. $sub);
     }
 
-    private static function writeConfiguration(IOInterface $io, array $config)
-    {
+    private function writeConfiguration(array $config){
         $contents = '<?php return ' . var_export($config, true) . ';';
 
-        $configFile = self::getPath('config.php');
+        $configFile = $this->getPath('config.php');
         $fp = fopen($configFile, 'w');
-        if (!$fp) {
-            $io->write('<error>Could not open configuration file (' . $configFile . ') for writing.</error>');
-            $io->write('Please create this file with the following contents: ');
-            $io->write($contents);
+        if (!$fp){
+            $this->io->write('<error>Could not open configuration file (' . $configFile . ') for writing.</error>');
+            $this->io->write('Please create this file with the following contents: ');
+            $this->io->write($contents);
         } else {
             fwrite($fp, $contents);
             fclose($fp);
         }
     }
 
-    private static function createVarDirectory(IOInterface $io)
-    {
-        $cacheDir = self::getPath('var/cache');
-        $storageDir = self::getPath('var/datastorage');
+    private function createVarDirectory(){
+        $cacheDir = $this->getPath('var/cache');
+        $storageDir = $this->getPath('var/datastorage');
 
         $result = true;
-        if (!file_exists($cacheDir)) {
+        if (!file_exists($cacheDir)){
             $result = mkdir($cacheDir, 0775, true);
         }
 
-        if (!file_exists($storageDir)) {
+        if (!file_exists($storageDir)){
             $result = $result && mkdir($storageDir, 0775, true);
         }
 
-        if (!$result) {
-            $io->write('<error>Could not create cache and storage directories.</error>');
-            $io->write('Please create the following directories and ensure they are writable by the server.');
-            $io->write('  ' . $cacheDir);
-            $io->write('  ' . $storageDir);
+        if (!$result){
+            $this->io->write('<error>Could not create cache and storage directories.</error>');
+            $this->io->write('Please create the following directories and ensure they are writable by the server.');
+            $this->io->write('  ' . $cacheDir);
+            $this->io->write('  ' . $storageDir);
         }
     }
 }
